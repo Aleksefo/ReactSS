440ba4370cb61d897a75f50e46a71a2cf1c79a2b
Alex Fomushkin <alex.fomushkin@honkio.com>
2017-08-19 22:42:32 +0300

    Nav skeleton and status bar overlaping fix

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
 import React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
+import {StyleSheet, Text, View, StatusBar} from 'react-native'
+import MainNavigator from './src/navigation/MainNavigator'
+
 const parseString = require('react-native-xml2js').parseString
 //convert that
 export default class App extends React.Component {
 	render() {
-		console.log("Ho")
-		fetch('https://www.youtube.com/feeds/videos.xml?channel_id=UCTFN4eJu6uAatDb6VR9XZag')
-			.then(response => response.text())
-			.then((response) => {
-				parseString(response, function (err, result) {
-					console.log(result.feed.entry)
-				});
-			}).catch((err) => {
-			// console.log('fetch', err)
-		})
+		// console.log("Ho")
+		// fetch('https://www.youtube.com/feeds/videos.xml?channel_id=UCTFN4eJu6uAatDb6VR9XZag')
+		// 	.then(response => response.text())
+		// 	.then((response) => {
+		// 		parseString(response, function (err, result) {
+		// 			console.log(result.feed.entry)
+		// 		});
+		// 	}).catch((err) => {
+		// 	// console.log('fetch', err)
+		// })
 		return (
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-				<Text>Changes you make will automatically reload.</Text>
-				<Text>Shake your phone to open the developer menu.</Text>
+			<View style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+				<MainNavigator />
 			</View>
+
 		);
 	}
 }
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { View, StyleSheet } from 'react-native'
+
+class HomeScreen extends Component {
+	state = {}
+
+	static navigationOptions = {
+		title: 'Welcome',
+	}
+
+	render() {
+		const {} = styles
+		return (
+	    <View></View>
+		)
+	}
+}
+
+const styles  = StyleSheet.create({
+	styleTop: {
+	},
+})
+
+export default HomeScreen
